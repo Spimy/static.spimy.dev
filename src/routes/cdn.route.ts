5e7e672d8f5a3956f0009b6a524f2c7280d51a78
Spimy <officialspyme@gmail.com>
2023-02-07 17:41:30 +0400

    Use authentication middleware

--- src/routes/cdn.route.ts
+++ src/routes/cdn.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { cdnUpload } from '../controllers/cdn.controller';
+import { isAuth } from '../middlewares/cdn.middleware';
 
 // Create router
 const router = Router();
 
 // Setup routes
-router.post('/upload', cdnUpload);
+router.post('/upload', isAuth, cdnUpload);
 
 export default router;
